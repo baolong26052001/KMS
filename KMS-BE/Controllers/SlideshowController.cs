61e3eebdef0db205042902f75bee8527afc88405
baolong26052001 <1959015@itec.hcmus.edu.vn>
2023-12-20 09:05:41 +0700

    Update SlideshowController.cs

--- KMS-BE/Controllers/SlideshowController.cs
+++ KMS-BE/Controllers/SlideshowController.cs
@@ -72,6 +72,10 @@ namespace KMS.Controllers
 
             if (startDate.HasValue && endDate.HasValue)
             {
+                
+                startDate = startDate.Value.Date.AddHours(0).AddMinutes(0).AddSeconds(0);
+                endDate = endDate.Value.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+
                 query += (parameters.Count == 0 ? " WHERE " : " AND ") + "startDate >= @startDate AND endDate <= @endDate";
                 parameters.Add(new SqlParameter("@startDate", startDate.Value));
                 parameters.Add(new SqlParameter("@endDate", endDate.Value));
