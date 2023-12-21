74e75c24880865b7f2dc66ee33c44f3d0cd692ce
baolong26052001 <1959015@itec.hcmus.edu.vn>
2023-12-21 11:16:31 +0700

    update BE

--- KMS-BE/Controllers/InsurancePackageController.cs
+++ KMS-BE/Controllers/InsurancePackageController.cs
@@ -3,6 +3,7 @@ using KMS.Models;
 using KMS.Tools;
 using System.Data;
 using Microsoft.Data.SqlClient;
+using System.Text;
 
 namespace KMS.Controllers
 {
@@ -22,7 +23,7 @@ namespace KMS.Controllers
         }
 
         [HttpGet]
-        [Route("ShowInsurancePackage")]
+        [Route("ShowInsurancePackage")] // show ra có bao nhiêu package (packageA,packageB,packageC,...)
         public JsonResult GetInsurancePackage()
         {
             string query = "select ipack.id, ipack.packageName, itype.typeName as insuranceType, ipack.packageGroup, ipack.duration, ipack.payType, ipack.annualFee, ipack.dateModified, ipack.dateCreated " +
@@ -34,7 +35,7 @@ namespace KMS.Controllers
         }
 
         [HttpGet]
-        [Route("ShowInsurancePackageDetail/{id}")]
+        [Route("ShowInsurancePackageDetail/{id}")] // khi ấn vào view package A, thì sẽ show ra benefit của package A
         public JsonResult GetInsurancePackageDetail(int id)
         {
             string query = "select b.id, b.content, b.coverage, b.description, ipack.packageName, itype.typeName, ipack.annualFee, b.dateModified, b.dateCreated " +
@@ -51,6 +52,27 @@ namespace KMS.Controllers
             else
             {
                 return new JsonResult("Insurance Package Detail not found");
+            }
+        }
+
+        [HttpGet]
+        [Route("ShowBenefit/{id}")] // ấn vào view benefit, sẽ ra benefit detail
+        public JsonResult GetBenefitDetail(int id)
+        {
+            string query = "select * " +
+                "from BenefitDetail " +
+                "where benefitId=@id";
+
+            SqlParameter parameter = new SqlParameter("@id", id);
+            DataTable table = _exQuery.ExecuteRawQuery(query, new[] { parameter });
+
+            if (table.Rows.Count > 0)
+            {
+                return new JsonResult(table);
+            }
+            else
+            {
+                return new JsonResult("Benefit Detail not found");
             }
         }
 
@@ -74,6 +96,119 @@ namespace KMS.Controllers
 
             return new JsonResult("Insurance Package added successfully");
         }
+
+        [HttpPut]
+        [Route("EditInsurancePackage/{id}")]
+        public JsonResult EditInsurancePackage(int id, [FromBody] InsurancePackage insurancePackage)
+        {
+            string query = "UPDATE InsurancePackage " +
+                           "SET packageName = @PackageName, insuranceType = @InsuranceType, duration = @Duration, " +
+                           "payType = @PayType, annualFee = @AnnualFee, dateModified = GETDATE() " +
+                           "WHERE id = @id";
+
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@id", id),
+                new SqlParameter("@PackageName", insurancePackage.PackageName),
+                new SqlParameter("@InsuranceType", insurancePackage.InsuranceType),
+                new SqlParameter("@Duration", insurancePackage.Duration),
+                new SqlParameter("@PayType", insurancePackage.PayType), // trả theo năm hoặc tháng
+                new SqlParameter("@AnnualFee", insurancePackage.AnnualFee),
+                
+            };
+
+            _exQuery.ExecuteRawQuery(query, parameters);
+
+            return new JsonResult("Insurance Package updated successfully");
+        }
+
+        [HttpDelete]
+        [Route("DeleteInsurancePackage")]
+        public JsonResult DeleteInsurancePackage([FromBody] List<int> insurancePackageIds)
+        {
+            if (insurancePackageIds == null || insurancePackageIds.Count == 0)
+            {
+                return new JsonResult("No insurance package IDs provided for deletion");
+            }
+
+            StringBuilder deleteQuery = new StringBuilder("DELETE FROM InsurancePackage WHERE id IN (");
+
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            for (int i = 0; i < insurancePackageIds.Count; i++)
+            {
+                string parameterName = "@InsurancePackageId" + i;
+                deleteQuery.Append(parameterName);
+
+                if (i < insurancePackageIds.Count - 1)
+                {
+                    deleteQuery.Append(", ");
+                }
+
+                parameters.Add(new SqlParameter(parameterName, insurancePackageIds[i]));
+            }
+
+            deleteQuery.Append(");");
+
+            _exQuery.ExecuteRawQuery(deleteQuery.ToString(), parameters.ToArray());
+
+            return new JsonResult("Insurance Package deleted successfully");
+        }
+
+        [HttpGet]
+        [Route("SearchInsurancePackage")]
+        public JsonResult SearchInsurancePackage(string searchQuery)
+        {
+            string query = "SELECT ipack.id, ipack.packageName, itype.typeName AS insuranceType, ipack.packageGroup, ipack.duration, ipack.payType, ipack.annualFee, ipack.dateModified, ipack.dateCreated " +
+               "FROM InsurancePackage ipack " +
+               "JOIN InsuranceType itype ON ipack.insuranceType = itype.id " +
+               "WHERE ipack.id LIKE @searchQuery OR " +
+               "ipack.packageName LIKE @searchQuery OR " +
+               "itype.typeName LIKE @searchQuery OR " +
+               "ipack.duration LIKE @searchQuery OR " +
+               "ipack.payType LIKE @searchQuery OR " +
+               "ipack.annualFee LIKE @searchQuery";
+
+
+
+            
+
+            SqlParameter parameter = new SqlParameter("@searchQuery", "%" + searchQuery + "%");
+            DataTable table = _exQuery.ExecuteRawQuery(query, new[] { parameter });
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet]
+        [Route("FilterInsurancePackage")]
+        public JsonResult FilterInsurancePackage([FromQuery] DateTime? startDate = null, [FromQuery] DateTime? endDate = null)
+        {
+            string query = "SELECT ipack.id, ipack.packageName, itype.typeName AS insuranceType, ipack.packageGroup, ipack.duration, ipack.payType, ipack.annualFee, ipack.dateModified, ipack.dateCreated " +
+               "FROM InsurancePackage ipack " +
+               "JOIN InsuranceType itype ON ipack.insuranceType = itype.id ";
+
+            List <SqlParameter> parameters = new List<SqlParameter>();
+
+
+
+            
+
+            if (startDate.HasValue && endDate.HasValue)
+            {
+
+                startDate = startDate.Value.Date.AddHours(0).AddMinutes(0).AddSeconds(0);
+                endDate = endDate.Value.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+                query += (parameters.Count == 0 ? " WHERE " : " AND ") + "ipack.dateCreated >= @startDate AND ipack.dateCreated <= @endDate";
+                parameters.Add(new SqlParameter("@startDate", startDate.Value));
+                parameters.Add(new SqlParameter("@endDate", endDate.Value));
+            }
+
+            DataTable table = _exQuery.ExecuteRawQuery(query, parameters.ToArray());
+
+            return new JsonResult(table);
+        }
+
 
     }
 }
---
+++ KMS-BE/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KMS.Models;
+using KMS.Tools;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace KMS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly KioskManagementSystemContext _dbcontext;
+        private IConfiguration _configuration;
+        private readonly ExecuteQuery _exQuery;
+
+        public TestController(IConfiguration configuration, KioskManagementSystemContext _context, ExecuteQuery exQuery)
+        {
+            _dbcontext = _context;
+            _configuration = configuration;
+            _exQuery = exQuery;
+        }
+
+        
+        
+
+    }
+}
