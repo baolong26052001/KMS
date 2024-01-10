d94a2e700a02f3cdc2779925b980696835eff6de
coftbred <nkluyen123@gmail.com>
2024-01-10 10:42:45 +0700

    update slide

--- KioskApp/Insurance/ViewModel/WelcomeVM.cs
+++ KioskApp/Insurance/ViewModel/WelcomeVM.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Insurance.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Insurance.ViewModel
 {
     internal class WelcomeVM :ViewModelBase
     {
+        public ICommand CancelCommand { get; set; }
 
+        public WelcomeVM() {
+            CancelCommand = new RelayCommand((parameter) =>
+            {
+                MainWindowVM.Instance.CurrentView = new InsuranceHomeVM();
+            });
+        }
     }
 }
