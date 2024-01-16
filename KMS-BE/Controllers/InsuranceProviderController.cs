870aa8da297b26878e4ce5846088518e575f2eeb
baolong26052001 <1959015@itec.hcmus.edu.vn>
2024-01-16 14:37:22 +0700

    xong api show/add/update/delete insurance provider

---
+++ KMS-BE/Controllers/InsuranceProviderController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KMS.Models;
+using KMS.Tools;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace KMS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InsuranceProviderController : ControllerBase
+    {
+        private readonly KioskManagementSystemContext _dbcontext;
+        private IConfiguration _configuration;
+        private readonly ExecuteQuery _exQuery;
+
+        public InsuranceProviderController(IConfiguration configuration, KioskManagementSystemContext _context, ExecuteQuery exQuery)
+        {
+            _dbcontext = _context;
+            _configuration = configuration;
+            _exQuery = exQuery;
+        }
+
+        [HttpGet]
+        [Route("ShowInsuranceProvider")]
+        public JsonResult GetInsuranceProvider()
+        {
+            string query = "SELECT * FROM InsuranceProvider";
+
+            DataTable table = _exQuery.ExecuteRawQuery(query);
+            return new JsonResult(table);
+        }
+
+        [HttpGet]
+        [Route("ShowInsuranceProvider/{id}")]
+        public JsonResult GetInsuranceProviderById(int id)
+        {
+            string query = "SELECT * FROM InsuranceProvider where id=@Id";
+
+            SqlParameter parameter = new SqlParameter("@Id", id);
+            DataTable table = _exQuery.ExecuteRawQuery(query, new[] { parameter });
+
+            if (table.Rows.Count > 0)
+            {
+                return new JsonResult(table);
+            }
+            else
+            {
+                return new JsonResult("Insurance Provider ID not found");
+            }
+        }
+
+        [HttpPost]
+        [Route("AddInsuranceProvider")]
+        public JsonResult AddInsuranceProvider([FromBody] InsuranceProvider insuranceProvider)
+        {
+            string query = "INSERT INTO InsuranceProvider (provider, email, dateCreated, dateModified) " +
+                           "VALUES (@Provider, @Email, GETDATE(), GETDATE())";
+
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Provider", insuranceProvider.Provider),
+                new SqlParameter("@Email", insuranceProvider.Email),
+            };
+
+            _exQuery.ExecuteRawQuery(query, parameters);
+
+            return new JsonResult("Insurance Provider added successfully");
+        }
+
+        [HttpPut]
+        [Route("EditInsuranceProvider/{id}")]
+        public JsonResult EditInsuranceProvider(int id, [FromBody] InsuranceProvider insuranceProvider)
+        {
+            string query = "UPDATE InsuranceProvider " +
+                           "SET provider = @Provider, email = @Email, dateModified = GETDATE() " +
+                           "WHERE id = @Id";
+
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Id", id),
+                new SqlParameter("@Provider", insuranceProvider.Provider),
+                new SqlParameter("@Email", insuranceProvider.Email),
+            };
+
+
+            _exQuery.ExecuteRawQuery(query, parameters);
+
+
+            return new JsonResult("Insurance Provider updated successfully");
+        }
+
+        [HttpDelete]
+        [Route("DeleteInsuranceProvider")]
+        public JsonResult DeleteInsuranceProvider([FromBody] List<int> insuranceProviderIds)
+        {
+            if (insuranceProviderIds == null || insuranceProviderIds.Count == 0)
+            {
+                return new JsonResult("No insurance provider IDs provided for deletion");
+            }
+
+            StringBuilder deleteQuery = new StringBuilder("DELETE FROM InsuranceProvider WHERE id IN (");
+            string query2 = "INSERT INTO TAudit (action, tableName, dateModified, dateCreated, isActive) VALUES ('Delete', 'InsuranceProvider', GETDATE(), GETDATE(), 1)";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            SqlParameter[] parameters2 = { };
+            for (int i = 0; i < insuranceProviderIds.Count; i++)
+            {
+                string parameterName = "@InsuranceProviderId" + i;
+                deleteQuery.Append(parameterName);
+
+                if (i < insuranceProviderIds.Count - 1)
+                {
+                    deleteQuery.Append(", ");
+                }
+
+                parameters.Add(new SqlParameter(parameterName, insuranceProviderIds[i]));
+            }
+
+            deleteQuery.Append(");");
+
+            _exQuery.ExecuteRawQuery(deleteQuery.ToString(), parameters.ToArray());
+            _exQuery.ExecuteRawQuery(query2, parameters2);
+
+            return new JsonResult("Insurance provider deleted successfully");
+        }
+
+    }
+}
