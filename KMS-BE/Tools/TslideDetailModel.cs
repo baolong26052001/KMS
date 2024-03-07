69943bbd93e5400469a76438b9e8e9c625b3207b
baolong26052001 <1959015@itec.hcmus.edu.vn>
2024-03-07 13:38:32 +0700

    Update TslideDetailModel.cs

--- KMS-BE/Tools/TslideDetailModel.cs
+++ KMS-BE/Tools/TslideDetailModel.cs
@@ -10,7 +10,7 @@ namespace KMS.Tools
         public string? Description { get; set; }
         public string? TypeContent { get; set; }
         public string? ContentUrl { get; set; }
-        public byte[]? ImageData { get; set; }
+        
         public string? ImageBase64 { get; set; }
 
         [NotMapped]
